9c6913882afabff13a1d18c901d4bc520264c684
KD Puvvadi <kdpuvvadi@gmail.com>
2021-11-16 19:52:58 +0530

    elevated permission

--- debloat.ps1
+++ debloat.ps1
@@ -1,6 +1,14 @@
 
 # Windows 11 Debloater
 
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
+
 function SetLeftStart {
     Write-Host "Setting Start Menu to Left"
     $StartKeyPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
