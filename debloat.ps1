e6f26edd3d799243483000183863caabec25b820
KD Puvvadi <kdpuvvadi@gmail.com>
2021-11-17 20:25:13 +0530

    cleanup

--- debloat.ps1
+++ debloat.ps1
@@ -19,59 +19,59 @@ Clear-Host
 $explorerPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 
 function SetLeftStart {
-    Write-Host "Setting Start Menu to Left"
+    Write-Host "Setting Start Menu to Left" -ForegroundColor Red
     
     if(Test-Path $explorerPath) {
         Set-ItemProperty -Path $explorerPath -Name TaskbarAl -Value 0
     }
     $checkStart = Get-ItemProperty -Path $explorerPath -Name TaskbarAl
     if( $checkStart.TaskbarAl -eq 0 ) {
-        Write-Host "Setting Start Menu to Left Completed." -ForegroundColor Green -BackgroundColor white
+        Write-Host "Done" -ForegroundColor Green -BackgroundColor white
     }
 
 }
 
 function RemoveChat {
-    Write-Host "Setting Start Menu to Left"
+    Write-Host "Setting Start Menu to Left" -ForegroundColor Red
 
     if(Test-Path $explorerPath) {
         Set-ItemProperty -Path $explorerPath -Name TaskbarMn -Value 0
     }
     $checkChat = Get-ItemProperty -Path $explorerPath -Name TaskbarMn
     if( $checkChat.TaskbarMn -eq 0 ) {
-        Write-Host "Removing chat from Start Menu Completed." -ForegroundColor Green -BackgroundColor white
+        Write-Host "Done" -ForegroundColor Green -BackgroundColor white
     }
 
 }
 
 function RemoveTaskView {
-    Write-Host "Removing Taskview Button"
+    Write-Host "Removing Taskview Button" -ForegroundColor Red
 
     if(Test-Path $explorerPath) {
         Set-ItemProperty -Path $explorerPath -Name ShowTaskViewButton -Value 0
     }
     $checkChat = Get-ItemProperty -Path $explorerPath -Name ShowTaskViewButton
     if( $checkChat.ShowTaskViewButton -eq 0 ) {
-        Write-Host "Removing Taskview Button Completed." -ForegroundColor Green -BackgroundColor white
+        Write-Host "Done" -ForegroundColor Green -BackgroundColor white
     }
 
 }
 
 function RemoveWidgeticon {
-    Write-Host "Removing Widget Button"
+    Write-Host "Removing Widget Button" -ForegroundColor Red
 
     if(Test-Path $explorerPath) {
         Set-ItemProperty -Path $explorerPath -Name TaskbarDa -Value 0
     }
     $checkChat = Get-ItemProperty -Path $explorerPath -Name TaskbarDa
     if( $checkChat.TaskbarDa -eq 0 ) {
-        Write-Host "Removing Widget Button Completed." -ForegroundColor Green -BackgroundColor white
+        Write-Host "Done" -ForegroundColor Green -BackgroundColor white
     }
 
 }
 
 function RemoveSearchIcon {
-    Write-Host "Removing search on Taskbar"
+    Write-Host "Removing search on Taskbar" -ForegroundColor Red
     $SearchKeyPath = "HKCU:Software\Microsoft\Windows\CurrentVersion\Search"
 
     if(Test-Path $explorerPath) {
@@ -79,13 +79,13 @@ function RemoveSearchIcon {
     }
     $checkSearch = Get-ItemProperty -Path $SearchKeyPath -Name SearchboxTaskbarMode
     if( $checkSearch.SearchboxTaskbarMode -eq 0 ) {
-        Write-Host "Remove search on Taskbar Completed." -ForegroundColor Green -BackgroundColor white
+        Write-Host "Done" -ForegroundColor Green -BackgroundColor white
     }
 
 }
 
 function DisableVBS {
-    Write-Host "Disabling Virtualization based Security"
+    Write-Host "Disabling Virtualization based Security" -ForegroundColor Red
     $VBSPath = "HKLM:SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity"
 
     if(Test-Path $VBSPath) {
@@ -93,7 +93,7 @@ function DisableVBS {
     }
     $checkSearch = Get-ItemProperty -Path $VBSPath -Name Enabled
     if( $checkSearch.Enabled -eq 0 ) {
-        Write-Host "Disabling Virtualization based Security Completed." -ForegroundColor Green -BackgroundColor white
+        Write-Host "Done" -ForegroundColor Green -BackgroundColor white
     }
 
 }
