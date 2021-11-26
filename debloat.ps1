dbde546814dea8c6c352a741c377a188c55ac369
KD <kdpuvvadi@gmail.com>
2021-11-26 12:33:12 +0530

    appearance lable visibilit

--- debloat.ps1
+++ debloat.ps1
@@ -85,8 +85,6 @@ $StartMenu.BackColor             = [System.Drawing.ColorTranslator]::FromHtml("#
 $Label2                          = New-Object system.Windows.Forms.Label
 $Label2.text                     = "Appearance"
 $Label2.AutoSize                 = $true
-$Label2.visible                  = $false
-$Label2.enabled                  = $false
 $Label2.width                    = 40
 $Label2.height                   = 20
 $Label2.location                 = New-Object System.Drawing.Point(20,150)
@@ -198,11 +196,20 @@ $Apps.Font                       = New-Object System.Drawing.Font('Microsoft San
 $Apps.ForeColor                  = [System.Drawing.ColorTranslator]::FromHtml("#ffffff")
 $Apps.BackColor                  = [System.Drawing.ColorTranslator]::FromHtml("#4a90e2")
 
+$OneDrive                        = New-Object system.Windows.Forms.Button
+$OneDrive.text                   = "Remove OneDrive"
+$OneDrive.width                  = 150
+$OneDrive.height                 = 45
+$OneDrive.location               = New-Object System.Drawing.Point(220,425)
+$OneDrive.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',12,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+$OneDrive.ForeColor              = [System.Drawing.ColorTranslator]::FromHtml("#ffffff")
+$OneDrive.BackColor              = [System.Drawing.ColorTranslator]::FromHtml("#4a90e2")
+
 $DebloatWindows11.controls.AddRange(@(
     $unpin,$disablecortana,$vbs,$DMode,$Apps,
     $LMode,$ListApps,$LeftMenu,$StartMenu,
     $EdgePDF,$Privacy,$FileExt,$RemoveKeys,
-    $Label1,$Label2,$Label3,$Label4
+    $Label1,$Label2,$Label3,$Label4,$OneDrive
     ))
 
 $unpin.Add_Click({ removeTaskIcon })
@@ -218,6 +225,7 @@ $Privacy.Add_Click({ Protect-Privacy })
 $FileExt.Add_Click({ ShowFileExt })
 $RemoveKeys.Add_Click({ Remove-Keys })
 $Apps.Add_Click({ InstallApps })
+$OneDrive.Add_Click({ UninstallOneDrive  })
 
 #Write your logic code here
 
@@ -234,6 +242,130 @@ function ShowFileExt {
     $extStatus = Get-ItemProperty $ExpPath -Name HideFileExt
     if ( $extStatus.HideFileExt -eq 0 ) {
         Write-Host "Done" -ForegroundColor Green -BackgroundColor white `n
+    }
+}
+
+Function UninstallOneDrive {
+
+    Write-Host "Checking for pre-existing files and folders located in the OneDrive folders..."
+    Start-Sleep 1
+    If (Test-Path "$env:USERPROFILE\OneDrive\*") {
+        Write-Host "Files found within the OneDrive folder! Checking to see if a folder named OneDriveBackupFiles exists."
+        Start-Sleep 1
+              
+        If (Test-Path "$env:USERPROFILE\Desktop\OneDriveBackupFiles") {
+            Write-Host "A folder named OneDriveBackupFiles already exists on your desktop. All files from your OneDrive location will be moved to that folder." 
+        }
+        else {
+            If (!(Test-Path "$env:USERPROFILE\Desktop\OneDriveBackupFiles")) {
+                Write-Host "A folder named OneDriveBackupFiles will be created and will be located on your desktop. All files from your OneDrive location will be located in that folder."
+                New-item -Path "$env:USERPROFILE\Desktop" -Name "OneDriveBackupFiles"-ItemType Directory -Force
+                Write-Host "Successfully created the folder 'OneDriveBackupFiles' on your desktop."
+            }
+        }
+        Start-Sleep 1
+        Move-Item -Path "$env:USERPROFILE\OneDrive\*" -Destination "$env:USERPROFILE\Desktop\OneDriveBackupFiles" -Force
+        Write-Host "Successfully moved all files/folders from your OneDrive folder to the folder 'OneDriveBackupFiles' on your desktop."
+        Start-Sleep 1
+        Write-Host "Proceeding with the removal of OneDrive."
+        Start-Sleep 1
+    }
+    Else {
+        Write-Host "Either the OneDrive folder does not exist or there are no files to be found in the folder. Proceeding with removal of OneDrive."
+        Start-Sleep 1
+        Write-Host "Enabling the Group Policy 'Prevent the usage of OneDrive for File Storage'."
+        $OneDriveKey = 'HKLM:Software\Policies\Microsoft\Windows\OneDrive'
+        If (!(Test-Path $OneDriveKey)) {
+            Mkdir $OneDriveKey
+            Set-ItemProperty $OneDriveKey -Name OneDrive -Value DisableFileSyncNGSC
+        }
+        Set-ItemProperty $OneDriveKey -Name OneDrive -Value DisableFileSyncNGSC
+    }
+
+    Write-Host "Uninstalling OneDrive. Please wait..."
+    
+
+    New-PSDrive  HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+    $onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
+    $ExplorerReg1 = "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+    $ExplorerReg2 = "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+    Stop-Process -Name "OneDrive*"
+    Start-Sleep 2
+    If (!(Test-Path $onedrive)) {
+        $onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+
+        New-PSDrive  HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+        $onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
+        $ExplorerReg1 = "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+        $ExplorerReg2 = "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
+        Stop-Process -Name "OneDrive*"
+        Start-Sleep 2
+        If (!(Test-Path $onedrive)) {
+            $onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+        }
+        Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
+        Start-Sleep 2
+        Write-Output "Stopping explorer"
+        Start-Sleep 1
+        taskkill.exe /F /IM explorer.exe
+        Start-Sleep 3
+        Write-Output "Removing leftover files"
+        Remove-Item "$env:USERPROFILE\OneDrive" -Force -Recurse
+        Remove-Item "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse
+        Remove-Item "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse
+        If (Test-Path "$env:SYSTEMDRIVE\OneDriveTemp") {
+            Remove-Item "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse
+        }
+        Write-Output "Removing OneDrive from windows explorer"
+        If (!(Test-Path $ExplorerReg1)) {
+            New-Item $ExplorerReg1
+        }
+        Set-ItemProperty $ExplorerReg1 System.IsPinnedToNameSpaceTree -Value 0 
+        If (!(Test-Path $ExplorerReg2)) {
+            New-Item $ExplorerReg2
+        }
+        Set-ItemProperty $ExplorerReg2 System.IsPinnedToNameSpaceTree -Value 0
+        Write-Output "Restarting Explorer that was shut down before."
+        Start-Process explorer.exe -NoNewWindow
+    
+        Write-Host "Enabling the Group Policy 'Prevent the usage of OneDrive for File Storage'."
+        $OneDriveKey = 'HKLM:Software\Policies\Microsoft\Windows\OneDrive'
+        If (!(Test-Path $OneDriveKey)) {
+            Mkdir $OneDriveKey 
+        }
+        Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
+        Start-Sleep 2
+        Write-Host "Stopping explorer"
+        Start-Sleep 1
+        taskkill.exe /F /IM explorer.exe
+        Start-Sleep 3
+        Write-Host "Removing leftover files"
+        If (Test-Path "$env:USERPROFILE\OneDrive") {
+            Remove-Item "$env:USERPROFILE\OneDrive" -Force -Recurse
+        }
+        If (Test-Path "$env:LOCALAPPDATA\Microsoft\OneDrive") {
+            Remove-Item "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse
+        }
+        If (Test-Path "$env:PROGRAMDATA\Microsoft OneDrive") {
+            Remove-Item "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse
+        }
+        If (Test-Path "$env:SYSTEMDRIVE\OneDriveTemp") {
+            Remove-Item "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse
+        }
+        Write-Host "Removing OneDrive from windows explorer"
+        If (!(Test-Path $ExplorerReg1)) {
+            New-Item $ExplorerReg1
+        }
+        Set-ItemProperty $ExplorerReg1 System.IsPinnedToNameSpaceTree -Value 0 
+        If (!(Test-Path $ExplorerReg2)) {
+            New-Item $ExplorerReg2
+        }
+        Set-ItemProperty $ExplorerReg2 System.IsPinnedToNameSpaceTree -Value 0
+        Write-Host "Restarting Explorer that was shut down before."
+        Start-Process explorer.exe -NoNewWindow
+        Write-Host "OneDrive has been successfully uninstalled!"
+        
+        Remove-item env:OneDrive
     }
 }
 
